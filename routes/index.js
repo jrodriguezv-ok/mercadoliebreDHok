d005b4101f174e9f209924c59a00571b190f0f14
Juan Viali <juanviali@gmail.comgit config --global user.email juanviali@gmail.comgit config --global user.email juanviali@gmail>
2020-09-10 16:57:35 -0300

    10/09  Index + Register + Login + CSS

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
-router.get("/login",function(req,res){
-  res.render("login");
+
+router.get("/login", function(req, res) {
+    res.render("login");
 });
 
-router.get("/register",function(req,res){
-  res.render("register");
+router.get("/register", function(req, res) {
+    res.render("register");
 });
+
 module.exports = router;
